9e6b098c65b047cb6ebfe71d0bf8bf8288d3c580
Natalia Purtova <3716987@gmail.com>
2020-05-10 11:07:35 +0300

    a part of Up button

---
+++ javascript/js.js
@@ -0,0 +1,21 @@
+let upButton = document.getElementById('buttonUp');
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 20) {
+        upButton.style.display = "block";
+    } else {
+        upButton.style.display = "none";
+    }
+}
+
+function goTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    }); // For Chrome, Firefox, IE and Opera
+}
+// for safari not documentElement, only body
